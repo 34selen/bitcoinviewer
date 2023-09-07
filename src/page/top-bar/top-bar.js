c06e84bacde62c742a2e25abfb48bf6ef9707adf
34selen <73600142+34selen@users.noreply.github.com>
2023-09-07 20:49:05 +0900

    top-bar

--- src/page/top-bar/top-bar.js
+++ src/page/top-bar/top-bar.js
@@ -9,24 +9,32 @@ function Top_bar(){
         검색:false
     });
     function onMouseEnterHandler(event){
-        setActiveMenu((prev)=>{
-            return{...prev, event.target.id:true};
-        })
+       
+        setActiveMenu((prevState)=>({
+            ...prevState,
+            [event.target.id]:true
+        }))
 
     }
     function onMouseLeaveHandler(event){
-        activeMenu[event.target.id]=false;
+        setActiveMenu((prevState)=>({
+            ...prevState,
+            [event.target.id]:false
+        }))
     }
+    setInterval(
+        console.log(activeMenu)
+    , 1000);
     return(
         <div className={styles.topBarContainer}>
             <Link to="/">
                 <img src="/favicon.png" className={styles.menuicon}></img>
             </Link>
             <ul className={styles.topBarButtonList}>
-                <li id="즐겨찾기"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)} onMouseLeave={(event)=>onMouseLeaveHandler(event)}>
+                <li id="즐겨찾기"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)} onMouseOut={(event)=>onMouseLeaveHandler(event)}>
                     즐겨찾기
                     {activeMenu["즐겨찾기"]?<div>aaaaa</div>:null}
-                    </li>
+                </li>
                 <li id="인기코인"className={styles.topBarButtonLeft}>인기코인</li>
                 <li id="회원정보"className={styles.topBarButtonRight}>회원정보</li>
                 <li id="검색"className={styles.topBarButtonRight}>검색</li>
