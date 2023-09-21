b727fbcfffd27f1e09f1800afb691539f4815a3a
34selen <73600142+34selen@users.noreply.github.com>
2023-09-21 19:17:52 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -5,40 +5,23 @@ import "./App.css"
 import { useEffect,useState } from 'react';
 import axios from 'axios';
 function App() {
-<<<<<<< Updated upstream
   const [coinData,setCoinData]=useState(null);
 
   useEffect(()=>{
     async function fetch() {
-        const response = await axios.get('https://api.bithumb.com/public/ticker/ALL');
-        setCoinData("aa");
-        console.log(coinData)
+        const response = await axios.get('https://api.bithumb.com/public/ticker/ALL')
+        setCoinData(response.data)
     };
     fetch();
-    
-
+  
 }, [])
-=======
-
-  const [data, setData] = useState([]);
-  	
-  useEffect(() => {
-  const fetchData = async() => {
-        const res = await fetch('https://https://api.coinpaprika.com/v1/tickers?quotes=KRW.typicode.com/news');
-        const result = res.json();
-        return result;
-      }	
-      
-      fetchData().then(res => setData(res));
-  }, []);
->>>>>>> Stashed changes
   return (
     <div>
       <Top_bar />
       <Routes>
         <Route path='/'element={<Main coinData={coinData}/>}/>
       </Routes>
-      <div>{data}</div>
+      <div>{coinData}</div>
     </div>
   );
 }
