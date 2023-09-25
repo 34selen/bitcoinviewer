c48d99ac0e86d9789aabb69dd89c57ba4481784f
34selen <73600142+34selen@users.noreply.github.com>
2023-09-25 09:14:43 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import { Route, Routes } from 'react-router-dom';
 import TopBar from "./page/top-bar/top-bar.js"
 import Main from "./page/main/main.js"
 import "./App.css"
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [coinData,setCoinData]=useState(null);
-useEffect(()=>{
-  async function fetchData(){
-    await fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
-    .then((res)=>console.log(res));
-  }
-  fetchData();
-},[])
+  const [coinData, setCoinData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW");
+    .then((res) => console.log(res));
+    }
+    fetchData();
+  }, [])
 
   return (
     <div>
-      <TopBar  />
+      <TopBar />
       <Routes>
-        <Route path='/'element={<Main/>}/>
+        <Route path='/' element={<Main />} />
       </Routes>
-  
+
 
     </div>
   );
