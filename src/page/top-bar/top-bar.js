e5e985614afff2b93d3b3e8be1a706173bddca40
34selen <73600142+34selen@users.noreply.github.com>
2023-09-28 15:11:00 +0900

    FF

--- src/page/top-bar/top-bar.js
+++ src/page/top-bar/top-bar.js
@@ -9,6 +9,7 @@ function TopBar({coinData}){
         회원정보:false,
         검색:false
     });
+    const [curInList,setCurInList]=useState(0);
     useEffect(()=>{
         if(coinData!=null){
             const coinNameArray = coinData.map(item => item.name);
@@ -16,29 +17,34 @@ function TopBar({coinData}){
         }
         }
     ,[coinData])
+    useEffect(()=>{
+        if(curInList===0){
+            setActiveMenu({
+                즐겨찾기:false,
+                코인목록:false,
+                회원정보:false,
+                검색:false
+            })
+           }
+    }
+    ,[curInList])
     const favorits=JSON.parse(window.localStorage.getItem('favorits'));
+    
     function onMouseEnterHandler(event){
-       
         setActiveMenu((prevState)=>({
             ...prevState,
             [event.target.id]:true
         }))
-
+       
     }
 
-    function onMouseLeaveHandler(event){
-        setActiveMenu((prevState)=>({
-            ...prevState,
-            [event.target.id]:false
-        }))
-    }
     return(
         <div className={styles.topBarContainer}>
             <Link to="/">
                 <img src="/favicon.png" className={styles.menuicon} alt="favicon"></img>
             </Link>
             <ul className={styles.topBarButtonList}>
-                <li id="즐겨찾기"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)} onMouseOut={(event)=>onMouseLeaveHandler(event)}>
+                <li id="즐겨찾기"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)} >
                     즐겨찾기
                     {activeMenu["즐겨찾기"]?<div className={styles.list}>
                         {favorits.map((favorit,index)=>(
@@ -48,14 +54,19 @@ function TopBar({coinData}){
                         ))}
                     </div>:null}
                 </li>
-                <li id="코인목록"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)} onMouseOut={(event)=>onMouseLeaveHandler(event)}>코인목록
-                {activeMenu["코인목록"]?<div className={styles.list}>
-                        {coinList.map((coinName,index)=>(
-                            <div key={index} onMouseEnter={(event)=>console.log(event)}>
-                                {coinName}
-                            </div>
-                        ))}
-                    </div>:null}
+                <li id="코인목록"className={styles.topBarButtonLeft} onMouseEnter={(event)=>onMouseEnterHandler(event)}>코인목록
+                {activeMenu["코인목록"]?<div>
+                        <div className={`${styles.list} custom-style`}>
+                            {coinList.map((coinName,index)=>(
+                                <div key={index} className={styles.topBarButtonLeft}>
+                                    {coinName}
+                                </div>
+                            ))}
+                        </div>
+                        <div className={styles.listActivate}>
+                    </div>
+                </div>
+                :null}
                 </li>
                 <li id="회원정보"className={styles.topBarButtonRight}>회원정보</li>
                 <li id="검색"className={styles.topBarButtonRight}>검색</li>
